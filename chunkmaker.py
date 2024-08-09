d301d55e2d0e87bbdc9a757ca8f6b425be0052b4
nivesh1000 <nivesh.nk1001@gmail.com>
2024-08-09 13:12:45 +0530

    added chroma database

--- chunkmaker.py
+++ chunkmaker.py
@@ -1,16 +1,16 @@
 def read_chunks(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         chunk = ''
+        prevline=''
         for line in file:
-            # print(line)
-            # print("----------------------------------------------------------")
+            if line == prevline:
+                continue
+            prevline=line
             
             if len(chunk) + len(line) <= 2048:
                 chunk += line
             else:
                 yield chunk.strip()
-                chunk = line  # Start a new chunk with the current line
-        
-        # Yield the last chunk if it's not empty
+                chunk = line
         if chunk:
             yield chunk.strip()
