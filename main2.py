dff72f9c6808f24dc133975c9348a9b95eae5ac4
nivesh1000 <nivesh.nk1001@gmail.com>
2024-08-23 16:07:16 +0530

    changed main2

--- main2.py
+++ main2.py
@@ -6,30 +6,6 @@ load_dotenv()
 from response import Response
 robj=Response()
 
-def chat_completion(self, user_query, similarity_result):
-    with open('system_prompt.txt', 'r') as file:
-        base_prompt = file.read()
-    deployment = os.getenv("DEPLOYEMENT")
-
-    final_prompt = base_prompt.format(
-        similarity_result=similarity_result,
-        user_query=user_query
-    )
-    completion = self.client.chat.completions.create(
-        model=deployment,
-        messages=[
-            {"role": "user", "content": final_prompt},
-        ],
-        max_tokens=800,
-        temperature=0.7,
-        top_p=0.95,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=None,
-        stream=False
-    )
-    return completion.choices[0].message.content
-
 # completion()
 
 def generate_response(query):
